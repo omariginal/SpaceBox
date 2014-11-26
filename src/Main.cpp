e4143f6275e22dd54220a3dd21235582c2c8379b
Jackson <jackson.noel17@hotmail.com>
2014-11-26 15:13:25 -0500

    mainplz

---
+++ src/Main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Nov 26, 2014
+ *      Author: Jackson
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+#include "Camera.hpp"
+#include "World.hpp"
+
+
